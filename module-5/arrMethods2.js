b5d35719cbebb788d5bad19ad2d33ddd57632995
praveen <praveen@mccoymart.com>
2023-05-14 15:25:47 +0530

    array sort and includes method. need to lookup sort even more

---
+++ module-5/arrMethods2.js
@@ -0,0 +1,15 @@
+/**Array includes and sort methods */
+
+Array Includes()
+the Array.includes() method is used to know either particular element is present in the array or not and accordingly
+it returbs true or false
+
+Syntax: array,includes(searchElement, start) returns true or false
+
+Array sort() method
+the arr.sort() method is used to sort the array in place in a given order according to the compare function. if the method is omitted then the array is sorted in ascending order.Arrayarr.sort(compareFunction)
+
+it returns the reference of sorted original array
+
+arr.sort(comparisionFunction)
+compareFunction(a,b)<0,>0,=0
