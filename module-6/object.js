cf893148da9396f20fc614843342efbc1dc1f7b5
praveen <praveenrawat1412@gmail.com>
2023-05-16 11:56:53 +0530

    objects intro

---
+++ module-6/object.js
@@ -0,0 +1,21 @@
+/**
+ * Objects --> {key: value} 🔥
+ */
+
+
+const personObj = {
+    name : "Praveen",
+    age: 26,
+    job: "React",
+    skills: ["html","css","js","react","c++","dsa"],
+    "is Admin": "true"
+} //object literal
+ console.log(personObj['is Admin'])
+
+// console.log(personObj["job"]) 
+// when using square bracket we have to put key property in string
+// after dot if we have to write mulyiple word property then we need square string notation
+
+
+
+
