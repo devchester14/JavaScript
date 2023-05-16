36d17bb68d04fb1e66599c3ba5a21c68b5f990e9
praveen <praveenrawat1412@gmail.com>
2023-05-16 19:04:59 +0530

    bind

---
+++ module-6/bind.js
@@ -0,0 +1,62 @@
+/**
+ * Bind
+ */
+
+/**
+ * Function Borrowing - Call, Apply And Bind
+ */
+
+const user1 = {
+    name: "praveen",
+    age: 90,
+    // sayHi(){
+    //     console.log(this.name);
+    // },
+};
+
+const user2 = {
+    name : "Rawat",
+    age: 100,
+    // sayHi(){
+    //     console.log(this.name);
+    // }
+}
+
+const user3 = {
+    name : "chester",
+    age: 28,
+    // sayHi(){
+    //     console.log(this.name); this is implicit binding
+    // }
+}
+
+// user1.sayHi();
+function sayHi(degree,year){
+    console.log(this.name,degree,year); /**this is written only for values of object else degree and year are param of fns */
+}
+
+// user1.sayHi()
+/**in JS call, apply and bind are methods of functions */
+// sayHi.call(user1,"BTECHCSE",2018);
+// sayHi.call(user2,"MTECHCSE",2018);
+
+/** whenever function is called, the value that is called is the this in the function object*/
+
+
+
+// APPLY IS SIMILAR TO CALL
+// sayHi.apply(user1,["BTECH",2018])
+// sayHi.apply(user2,["MTECH",2022])
+
+
+//first parameter  has to be object borrowing the fn, second  are params fn takes. with cal;l first param is object next are params of fn args but in apply it has 
+// array for. this is also called explicit binding
+
+
+/**BIND */
+
+const result = sayHi.bind(user3,"B.tech - CS")
+result(2022)
+
+// call with we pass args first, apply we have array args. with bind also we pass parameters.
+// bind returns function        THis is explicit binding as well
