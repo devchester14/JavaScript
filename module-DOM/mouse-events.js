f7ffa62efdbde5baa1e043026c59eb34fe5b77b3
praveen rawat <praveenrawat1412@gmail.com>
2023-06-26 14:41:03 +0530

    mouse evenets

---
+++ module-DOM/mouse-events.js
@@ -0,0 +1,9 @@
+/**
+ * Mouse Events
+ */
+
+input.addEventListener("mousedown",(event)=>{console.log(event.button)})
+
+event.pageX;
+event.pageY
+clientX is only for visible area page x and Y is for the entire page
